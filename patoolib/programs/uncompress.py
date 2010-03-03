8d02fdfe8fbc506f27578468f300e681b12de8f4
Bastian Kleineidam <calvin@debian.org>
2010-03-03 21:10:11 +0100

    Remove unused imports

--- patoolib/programs/uncompress.py
+++ patoolib/programs/uncompress.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Archive commands for the uncompress.real program."""
-import os
 from .. import util
 
 
--- patoolib/programs/xz.py
+++ patoolib/programs/xz.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Archive commands for the xz program."""
-import os
 from .. import util
 
 
