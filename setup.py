6d94cca38886672ffd7fe3805c0bb50c4d0c29e6
Bastian Kleineidam <calvin@debian.org>
2012-01-30 20:54:08 +0100

    Updated version and changelog.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import os
 from distutils.core import setup
 
 AppName = "patool"
-AppVersion = "0.13"
+AppVersion = "0.14"
 MyName = "Bastian Kleineidam"
 MyEmail = "calvin@users.sourceforge.net"
 
