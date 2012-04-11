470fb63c2d1469f0d8d265cc3e5b891f61d5d482
Bastian Kleineidam <calvin@debian.org>
2012-04-11 18:21:22 +0200

    Fix RPM installation.

--- install-rpm.sh
+++ install-rpm.sh
@@ -2,14 +2,19 @@ python setup.py install --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
 # 'brp-compress' compresses the manpages without distutils knowing.
 # The sed scripts append ".gz", ".bz2" or ".xz" suffixes to the affected
 # manpage filenames.
+RPM_MANDIR=usr/share/man
+RPM_LOCALMANDIR=usr/local/share/man
 
-if [ -n "$( ls $RPM_BUILD_ROOT/usr/share/man/man*/*.bz2 2>/dev/null )" ]; then
+if [ -n "$( ls $RPM_BUILD_ROOT/$RPM_MANDIR/man*/*.bz2 2>/dev/null )" \
+  -o -n "$( ls $RPM_BUILD_ROOT/$RPM_LOCALMANDIR/man*/*.bz2 2>/dev/null )" ]; then
     # add .bz2 suffix
     sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.bz2@g' INSTALLED_FILES
-elif [ -n "$( ls $RPM_BUILD_ROOT/usr/share/man/man*/*.xz 2>/dev/null )" ]; then
+elif [ -n "$( ls $RPM_BUILD_ROOT/$RPM_MANDIR/man*/*.xz 2>/dev/null )" \
+    -o -n "$( ls $RPM_BUILD_ROOT/$RPM_LOCALMANDIR/man*/*.xz 2>/dev/null )" ]; then
     # add .xz suffix
     sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.xz@g' INSTALLED_FILES
-else
-    # assume that man pages have a ".gz" suffix
+elif [ -n "$( ls $RPM_BUILD_ROOT/$RPM_MANDIR/man*/*.gz 2>/dev/null )" \
+    -o -n "$( ls $RPM_BUILD_ROOT/$RPM_LOCALMANDIR/man*/*.gz 2>/dev/null )" ]; then
+    # add .gz suffix
     sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
 fi
