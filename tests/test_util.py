42ad8961713f3003e8f89238aaf9f74fbc84141e
Bastian Kleineidam <calvin@debian.org>
2012-05-11 21:15:59 +0200

    Remove unused import.

--- tests/test_util.py
+++ tests/test_util.py
@@ -15,7 +15,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import unittest
 import os
-import shutil
 from patoolib import util
 
 class UtilTest (unittest.TestCase):
