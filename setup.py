3392db7e2c00fe932ac4f2c968bb76dbbadac28c
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-02-28 07:34:51 +0100

    Add app dir to PATH.

--- setup.py
+++ setup.py
@@ -170,11 +170,15 @@ class InnoScript:
         print("[Setup]", file=fd)
         print("AppName=%s" % self.name, file=fd)
         print("AppVerName=%s %s" % (self.name, self.version), file=fd)
+        print("ChangesEnvironment=true", file=fd)
         print(r"DefaultDirName={pf}\%s" % self.name, file=fd)
         print("DefaultGroupName=%s" % self.name, file=fd)
         print("OutputBaseFilename=%s" % self.distfilebase, file=fd)
         print("OutputDir=..", file=fd)
         print("SetupIconFile=%s" % self.icon, file=fd)
+        print(file=fd)
+        print("[Tasks]", file=fd)
+        print("Name: modifypath; Description: Add app directory to environment PATH; Flags: unchecked", file=fd)
         print(file=fd)
         # List of source files
         files = self.windows_exe_files + \
@@ -190,12 +194,30 @@ class InnoScript:
         for path in self.windows_exe_files:
             print(r'Name: "{group}\%s"; Filename: "{app}\%s"' %
                   (self.name, path), file=fd)
+        if self.console_exe_files:
+            print(r'Name: "{group}\Patool command prompt"; Filename: "cmd.exe"', file=fd)
         print(r'Name: "{group}\Uninstall %s"; Filename: "{uninstallexe}"' % self.name, file=fd)
         print(file=fd)
         # Uninstall optional log files
         print('[UninstallDelete]', file=fd)
         print(r'Type: files; Name: "{pf}\%s\patool*.exe.log"' % self.name, file=fd)
         print(file=fd)
+        # Add app dir to PATH
+        print("[Code]", file=fd)
+        print("""\
+const
+    ModPathName = 'modifypath';
+    ModPathType = 'user'; 
+
+function ModPathDir(): TArrayOfString; 
+begin 
+    setArrayLength(Result, 1) 
+    Result[0] := ExpandConstant('{app}'); 
+end; 
+#include "modpath.iss"
+""", file=fd)
+        shutil.copy(r"scripts\modpath.iss", "dist")
+
 
     def compile (self):
         """Compile Inno script with iscc.exe."""
