e5a4d8b31747c41cb941faa33219ad916ca012b6
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-04-08 18:26:10 +0200

    Use shutil.which() and fix version.

--- patoolib/util.py
+++ patoolib/util.py
@@ -23,10 +23,64 @@ import mimetypes
 import tempfile
 import time
 import traceback
-from distutils.spawn import find_executable
+try:
+    from shutil import which
+except ImportError:
+    # from Python 3.3
+    def which(cmd, mode=os.F_OK | os.X_OK, path=None):
+        """Given a command, mode, and a PATH string, return the path which
+        conforms to the given mode on the PATH, or None if there is no such
+        file.
+        `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+        of os.environ.get("PATH"), or can be overridden with a custom search
+        path.
+        """
+        # Check that a given file can be accessed with the correct mode.
+        # Additionally check that `file` is not a directory, as on Windows
+        # directories pass the os.access check.
+        def _access_check(fn, mode):
+            return (os.path.exists(fn) and os.access(fn, mode)
+                    and not os.path.isdir(fn))
+        # If we're given a path with a directory part, look it up directly rather
+        # than referring to PATH directories. This includes checking relative to the
+        # current directory, e.g. ./script
+        if os.path.dirname(cmd):
+            if _access_check(cmd, mode):
+                return cmd
+            return None
+        path = (path or os.environ.get("PATH", os.defpath)).split(os.pathsep)
+        if sys.platform == "win32":
+            # The current directory takes precedence on Windows.
+            if not os.curdir in path:
+                path.insert(0, os.curdir)
+            # PATHEXT is necessary to check on Windows.
+            pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+            # See if the given file matches any of the expected path extensions.
+            # This will allow us to short circuit when given "python.exe".
+            # If it does match, only test that one, otherwise we have to try
+            # others.
+            if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+                files = [cmd]
+            else:
+                files = [cmd + ext for ext in pathext]
+        else:
+            # On other platforms you don't have things like PATHEXT to tell you
+            # what file suffixes are executable, so just pass on cmd as-is.
+            files = [cmd]
+        seen = set()
+        for dir in path:
+            normdir = os.path.normcase(dir)
+            if not normdir in seen:
+                seen.add(normdir)
+                for thefile in files:
+                    name = os.path.join(dir, thefile)
+                    if _access_check(name, mode):
+                        return name
+        return None
+
 
 AppName = "patool"
-App = "%s 1.0" % AppName
+App = "%s 1.1" % AppName
 SupportUrl = "https://github.com/wummel/patool/issues"
 
 # internal MIME database
@@ -482,11 +536,14 @@ def p7zip_supports_rar ():
 @memoized
 def find_program (program):
     """Look for program in environment PATH variable."""
-    path = os.environ['PATH']
     if os.name == 'nt':
+        path = os.environ['PATH']
         path = append_to_path(path, get_nt_7z_dir())
         path = append_to_path(path, get_nt_mac_dir())
-    return find_executable(program, path=path)
+    else:
+        # use default path
+        path = None
+    return which(program, path=path)
 
 
 def append_to_path (path, directory):
