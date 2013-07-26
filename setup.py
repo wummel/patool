45a0d93dec497771fbb9dc0a74d0f7d0563bbf4f
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-07-26 22:32:17 +0200

    Bump up version.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ from distutils import util
 from distutils.file_util import write_file
 
 AppName = "Patool"
-AppVersion = "1.3"
+AppVersion = "1.4"
 MyName = "Bastian Kleineidam"
 MyEmail = "bastian.kleineidam@web.de"
 
