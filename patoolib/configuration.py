4a2c3d55ce559b78ed1b4e38ed84382796394d25
Bastian Kleineidam <bastian.kleineidam@web.de>
2015-12-05 12:12:57 +0100

    Fix support URL

--- patoolib/configuration.py
+++ patoolib/configuration.py
@@ -7,10 +7,10 @@ import _patool_configdata as configdata
 Version = configdata.version
 ReleaseDate = configdata.release_date
 AppName = configdata.name
-App = AppName+" "+Version
+App = AppName+u" "+Version
 Author = configdata.author
 Maintainer = configdata.maintainer
-Copyright = "Copyright (C) 2004-2015 " + Author
+Copyright = u"Copyright (C) 2004-2015 " + Author
 Url = configdata.url
-SupportUrl = Url + "issues"
+SupportUrl = u"https://github.com/wummel/patool/issues/"
 Email = configdata.author_email
