ffdcb9da6b7c0b8d5af5ab77cbf756724b3d8e8f
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2021-04-04 11:58:35 +0200

    Use standard setuptools to build/install

--- patoolib/configuration.py
+++ patoolib/configuration.py
@@ -2,15 +2,21 @@
 """
 Define basic configuration data like version or application name.
 """
-import _patool_configdata as configdata
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata  # type: ignore
 
-Version = configdata.version
-ReleaseDate = configdata.release_date
-AppName = configdata.name
-App = AppName+u" "+Version
-Author = configdata.author
-Maintainer = configdata.maintainer
-Copyright = u"Copyright (C) 2004-2015 " + Author
-Url = configdata.url
-SupportUrl = u"https://github.com/wummel/patool/issues/"
-Email = configdata.author_email
+meta = metadata.metadata("patool")
+
+Version = metadata.version("patool")
+ReleaseDate = "17.1.2016"  # from first line of doc/changelog.txt
+AppName = meta["Name"]
+App = AppName+" "+Version
+Author = meta["Author"]
+Maintainer = meta["Maintainer"]
+Copyright = "Copyright (C) 2004-2015 " + Author
+Url = meta["Home-page"]
+SupportUrl = "https://github.com/wummel/patool/issues/"
+Email = meta["Author-email"]
--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# -*- coding: iso-8859-1 -*-
 # Copyright (C) 2010-2016 Bastian Kleineidam
 #
 # This program is free software: you can redistribute it and/or modify
@@ -17,170 +15,11 @@
 """
 Setup file for the distuils module.
 """
-from __future__ import print_function
-import sys
-if not hasattr(sys, "version_info") or sys.version_info < (3, 5):
-    raise SystemExit("This program requires Python 3.5 or later.")
-import os
-import re
-from setuptools import setup
-from distutils.core import Distribution
-from distutils.command.install_lib import install_lib
-from distutils import util
-from distutils.file_util import write_file
-
-AppName = "patool"
-AppVersion = "1.12"
-MyName = "Bastian Kleineidam"
-MyEmail = "bastian.kleineidam@web.de"
-
-
-def normpath (path):
-    """Norm a path name to platform specific notation."""
-    return os.path.normpath(path)
-
-
-def cnormpath (path):
-    """Norm a path name to platform specific notation and make it absolute."""
-    path = normpath(path)
-    if os.name == 'nt':
-        # replace slashes with backslashes
-        path = path.replace("/", "\\")
-    if not os.path.isabs(path):
-        path = normpath(os.path.join(sys.prefix, path))
-    return path
-
-
-release_ro = re.compile(r"\(released (.+)\)")
-def get_release_date ():
-    """Parse and return relase date as string from doc/changelog.txt."""
-    fname = os.path.join("doc", "changelog.txt")
-    release_date = "unknown"
-    with open(fname) as fd:
-        # the release date is on the first line
-        line = fd.readline()
-        mo = release_ro.search(line)
-        if mo:
-            release_date = mo.groups(1)
-    return release_date
-
-
-data_files = []
-if os.name == 'nt':
-    data_files.append(('share', ['doc/patool.txt']))
-else:
-    data_files.append(('share/man/man1', ['doc/patool.1']))
-
-
-class MyInstallLib (install_lib, object):
-    """Custom library installation."""
-
-    def install (self):
-        """Install the generated config file."""
-        outs = super(MyInstallLib, self).install()
-        infile = self.create_conf_file()
-        outfile = os.path.join(self.install_dir, os.path.basename(infile))
-        self.copy_file(infile, outfile)
-        outs.append(outfile)
-        return outs
-
-    def create_conf_file (self):
-        """Create configuration file."""
-        cmd_obj = self.distribution.get_command_obj("install")
-        cmd_obj.ensure_finalized()
-        # we have to write a configuration file because we need the
-        # <install_data> directory (and other stuff like author, url, ...)
-        # all paths are made absolute by cnormpath()
-        data = []
-        for d in ['purelib', 'platlib', 'lib', 'headers', 'scripts', 'data']:
-            attr = 'install_%s' % d
-            if cmd_obj.root:
-                # cut off root path prefix
-                cutoff = len(cmd_obj.root)
-                # don't strip the path separator
-                if cmd_obj.root.endswith(os.sep):
-                    cutoff -= 1
-                val = getattr(cmd_obj, attr)[cutoff:]
-            else:
-                val = getattr(cmd_obj, attr)
-            if attr == 'install_data':
-                cdir = os.path.join(val, "share", AppName)
-                data.append('config_dir = %r' % cnormpath(cdir))
-            elif attr == 'install_lib':
-                if cmd_obj.root:
-                    _drive, tail = os.path.splitdrive(val)
-                    if tail.startswith(os.sep):
-                        tail = tail[1:]
-                    self.install_lib = os.path.join(cmd_obj.root, tail)
-                else:
-                    self.install_lib = val
-            data.append("%s = %r" % (attr, cnormpath(val)))
-        self.distribution.create_conf_file(data, directory=self.install_lib)
-        return self.get_conf_output()
-
-    def get_conf_output (self):
-        """Get filename for distribution configuration file."""
-        return self.distribution.get_conf_filename(self.install_lib)
-
-    def get_outputs (self):
-        """Add the generated config file to the list of outputs."""
-        outs = super(MyInstallLib, self).get_outputs()
-        conf_output = self.get_conf_output()
-        outs.append(conf_output)
-        if self.compile:
-            outs.extend(self._bytecode_filenames([conf_output]))
-        return outs
-
-
-class MyDistribution (Distribution, object):
-    """Custom distribution class generating config file."""
-
-    def __init__ (self, attrs):
-        """Set console and windows scripts."""
-        super(MyDistribution, self).__init__(attrs)
-        self.console = ['patool']
-
-    def run_commands (self):
-        """Generate config file and run commands."""
-        cwd = os.getcwd()
-        data = []
-        data.append('config_dir = %r' % os.path.join(cwd, "config"))
-        data.append("install_data = %r" % cwd)
-        data.append("install_scripts = %r" % cwd)
-        self.create_conf_file(data)
-        super(MyDistribution, self).run_commands()
-
-    def get_conf_filename (self, directory):
-        """Get name for config file."""
-        return os.path.join(directory, "_%s_configdata.py" % self.get_name())
-
-    def create_conf_file (self, data, directory=None):
-        """Create local config file from given data (list of lines) in
-        the directory (or current directory if not given)."""
-        data.insert(0, "# this file is automatically created by setup.py")
-        data.insert(0, "# -*- coding: iso-8859-1 -*-")
-        if directory is None:
-            directory = os.getcwd()
-        filename = self.get_conf_filename(directory)
-        # add metadata
-        metanames = ("name", "version", "author", "author_email",
-                     "maintainer", "maintainer_email", "url",
-                     "license", "description", "long_description",
-                     "keywords", "platforms", "fullname", "contact",
-                     "contact_email")
-        for name in metanames:
-            method = "get_" + name
-            val = getattr(self.metadata, method)()
-            data.append("%s = %r" % (name, val))
-        data.append('release_date = "%s"' % get_release_date())
-        # write the config file
-        util.execute(write_file, (filename, data),
-                     "creating %s" % filename, self.verbose >= 1, self.dry_run)
-
+import setuptools
 
 args = dict(
-    name = AppName,
-    version = AppVersion,
+    name = "patool",
+    version = "1.12",
     description = "portable archive file manager",
     long_description = """Various archive formats can be created, extracted, tested, listed,
 searched, compared and repacked by patool. The advantage of patool
@@ -204,15 +43,12 @@ The archive formats TAR, ZIP, BZIP2 and GZIP
 are supported natively and do not require helper applications to be
 installed.
 """,
-    author = MyName,
-    author_email = MyEmail,
-    maintainer = MyName,
-    maintainer_email = MyEmail,
+    author = "Bastian Kleineidam",
+    author_email = "bastian.kleineidam@web.de",
+    maintainer = "Bastian Kleineidam",
+    maintainer_email = "bastian.kleineidam@web.de",
     license = "GPL",
     url = "http://wummel.github.io/patool/",
-    packages = ['patoolib', 'patoolib.programs'],
-    data_files = data_files,
-    scripts = ['patool'],
     keywords = "archiver,archive,compression,commandline,manager",
     classifiers = [
         'Environment :: Console',
@@ -227,9 +63,13 @@ installed.
         'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
     ],
-    distclass = MyDistribution,
-    cmdclass = {
-        'install_lib': MyInstallLib,
-    },
+    install_requires=[
+        'importlib-metadata ~= 1.0 ; python_version < "3.8"',
+    ],
+    #packages = ['patoolib', 'patoolib.programs'],
+    packages = setuptools.find_packages(),
+    entry_points = {'console_scripts': ['patool = patool.patool:main']},
+    python_requires=">=3.5",
 )
-setup(**args)
+
+setuptools.setup(**args)
