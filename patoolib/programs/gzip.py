244a48ad1cc7cc4aff3c65d08066c21b78c8f173
Bastian Kleineidam <bastian.kleineidam@web.de>
2023-11-14 00:23:21 +0100

    Do not extract original filenames with .Z archives

--- patoolib/programs/gzip.py
+++ patoolib/programs/gzip.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Archive commands for the gzip program."""
-from . import test_singlefile_standard
+from . import extract_singlefile_standard, test_singlefile_standard
 from .. import util, fileutil, log
 import os
 import subprocess
@@ -30,7 +30,7 @@ def extract_gzip(archive, compression, cmd, verbosity, interactive, outdir):
         util.shell_quote(outfile)])
     return (cmdlist, {'shell': True})
 
-extract_compress = extract_gzip
+extract_compress = extract_singlefile_standard
 
 
 def get_original_filename(cmd, outdir, archive):
