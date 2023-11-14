930951d7872fcad789d8a7aebd285407087f384d
Bastian Kleineidam <bastian.kleineidam@web.de>
2023-11-14 21:00:17 +0100

    Updated homepage metadata

--- doc/web/source/conf.py
+++ doc/web/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "patool"
-version = "1.15.0"
+version = "2.0.0"
 author = "Bastian Kleineidam"
 copyright = '2023 ' + author
 
