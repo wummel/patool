678374239df66012222414b797d5c5ded8ab79d3
Bastian Kleineidam <bastian.kleineidam@web.de>
2024-06-26 22:35:07 +0200

    add local .env file

--- doc/web/source/conf.py
+++ doc/web/source/conf.py
@@ -9,7 +9,7 @@ import sys
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "patool"
-version = "2.2.0"
+version = "2.3.0"
 author = "Bastian Kleineidam"
 copyright = '2024 ' + author
 
