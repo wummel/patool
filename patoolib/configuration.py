e82ba84df5cb69cccd9e72376d3618c1ea75dfe4
Bastian Kleineidam <bastian.kleineidam@web.de>
2024-11-19 07:20:25 +0100

    Bump minor version for option change

--- patoolib/configuration.py
+++ patoolib/configuration.py
@@ -2,7 +2,7 @@
 """Define basic configuration data like version or application name."""
 
 AppName: str = "patool"
-Version: str = "3.0.4"
+Version: str = "3.1.0"
 MyName: str = "Bastian Kleineidam"
 MyEmail: str = "bastian.kleineidam@web.de"
 
