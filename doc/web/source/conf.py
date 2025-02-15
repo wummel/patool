e2d97fbcb4842a5eeefcd7b70f1dfb0e0dd33c47
Bastian Kleineidam <bastian.kleineidam@web.de>
2025-02-15 21:04:41 +0100

    Updated version

--- doc/web/source/conf.py
+++ doc/web/source/conf.py
@@ -9,7 +9,7 @@ import sys
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "patool"
-version = "3.1.2"
+version = "3.1.3"
 author = "Bastian Kleineidam"
 copyright = '2024 ' + author
 
