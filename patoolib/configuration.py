18076703966306c4edb7911cb5df46de2e77d55f
Bastian Kleineidam <bastian.kleineidam@web.de>
2025-12-11 19:20:34 +0100

    Updated version

--- patoolib/configuration.py
+++ patoolib/configuration.py
@@ -2,7 +2,7 @@
 """Define basic configuration data like version or application name."""
 
 AppName: str = "patool"
-Version: str = "4.0.3"
+Version: str = "4.0.4"
 MyName: str = "Bastian Kleineidam"
 MyEmail: str = "bastian.kleineidam@web.de"
 
